06e530650b9972d75d10fa9da74903318fb36cfb
Joseph Obiagba <j.i.obiagba2016@gmail.com>
2019-04-29 17:04:05 +0100

    This is a 3rd version of my JavaScript site. See how button and Axios have been used to test some other features on the site!

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//this function formats the response to be in proper html format
+function postJoke(response) {
+    return '<p id="joke" class="joke">' + response.data.value.joke + '</p>';
+};
+
+//this function formats the error in proper html
+function postError(error) {
+    return '<p id="joke" class="joke">' + error + '</p>';
+}
+
+//This function will be called in index.html
+function getJoke() {
+    //first get element
+    let jokeElement = document.getElementById("columnheader2");
+    jokeElement.innerHTML = "";
+
+    //Call in formating functions in axios object. Axios is called using CDN in index.html
+    axios.get('http://api.icndb.com/jokes/random?')
+    .then(response => {
+        jokeElement.innerHTML = postJoke(response);
+    })
+    .catch(error => {
+        jokeElement.innerHTML = postError(error);
+    });
+}
